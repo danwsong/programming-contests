3118a7449e94939314a177781e6b6281a4691c73
daniel song <44812932+danwsong@users.noreply.github.com>
2019-12-28 22:08:03 -0500

    Update S5.cpp

--- CCC/2015/S5.cpp
+++ CCC/2015/S5.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 #define MAXN 3001
-#define MAXM 100
+#define MAXM 101
 
 int N, M, A[MAXN], B[MAXM], dp[MAXN][2][MAXM][MAXM];
 
@@ -20,26 +20,28 @@ int solve(int pos, int take, int l, int r) {
     }
     if (take) {
         dp[pos][take][l][r] = max(solve(pos, 0, l, r), A[pos] + solve(pos + 1, 0, l, r));
-        if (l <= r)
+        if(l <= r)
             dp[pos][take][l][r] = max(dp[pos][take][l][r], B[r] + solve(pos, 0, l, r - 1));
     } else {
         dp[pos][take][l][r] = solve(pos + 1, 1, l, r);
-        if (l <= r)
+        if(l <= r)
             dp[pos][take][l][r] = max(dp[pos][take][l][r], solve(pos, 1, l + 1, r));
     }
     return dp[pos][take][l][r];
 }
 
 int main() {
+    memset(dp, -1, sizeof(dp));
+
     cin >> N;
     for (int i = 0; i < N; i++)
         cin >> A[i];
     cin >> M;
     for (int i = 0; i < M; i++)
         cin >> B[i];
+
     sort(B, B + M);
 
-    memset(dp, -1, sizeof(dp));
     cout << solve(0, 1, 0, M - 1) << endl;
 
     return 0;
